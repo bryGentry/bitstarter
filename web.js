6cfffa880f35d190fdcedc37d703d65a9585a022
bryGentry <bryangentry@gmail.com>
2013-07-16 10:48:20 +0000

    updated web.js again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
  var contents = fs.readFileSync('index.html');
-console.log(contents.toString());
+response.write(contents.toString());
 
 });
 
