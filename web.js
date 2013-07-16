0b22e3c95633ec059a001cbd4e25564a54199cad
bryGentry <bryangentry@gmail.com>
2013-07-16 03:20:53 +0000

    tried again to fix web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
  var contents = fs.readFileSync('index.html');
-contents.toString();
+console.log(contents.toString());
 
 });
 
